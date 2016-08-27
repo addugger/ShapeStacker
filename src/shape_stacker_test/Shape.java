d89e40010ccf4f1692049d1913102e43f4c26614
Aaron Dugger <addugger@gmail.com>
2016-08-27 17:22:13 -0400

    Initial git commit. Does include just initiated Shape.java.

---
+++ src/shape_stacker_test/Shape.java
@@ -0,0 +1,13 @@
+/**
+ * Shape must be extended (or at least extended further up the chain) for all
+ * other shapes to be used by ShapeStacker.
+ */
+package shape_stacker_test;
+
+/**
+ * @author Delane
+ *
+ */
+public abstract class Shape {
+
+}
