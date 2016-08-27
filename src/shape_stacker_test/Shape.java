b1267933e88756ce60cb1db6468747f7cda4cea9
Aaron Dugger <addugger@gmail.com>
2016-08-27 17:34:42 -0400

    Completed initial version of Shape.java

--- src/shape_stacker_test/Shape.java
+++ src/shape_stacker_test/Shape.java
@@ -1,13 +1,44 @@
 /**
- * Shape must be extended (or at least extended further up the chain) for all
- * other shapes to be used by ShapeStacker.
+ * Shape is the base class for all other shapes to be used by ShapeStacker.
  */
 package shape_stacker_test;
 
 /**
- * @author Delane
+ * @author Aaron Dugger
+ * 
  *
  */
 public abstract class Shape {
+	/**
+	 * Number of corners the shape has. This will be used to help determine
+	 * which shapes are "larger" than others when they have equal area.
+	 */
+	private int cornerCnt;
+	
+	/**
+	 * The only value all the shapes will definitely have is cornerCnt, so
+	 * that is set via Shape's constructor.	
+	 */
+	public Shape(int cornerCnt) {
+		this.setCornerCnt(cornerCnt);
+	}
+	
+	/**
+	 * Each shape class must implement the getArea() method.
+	 */
+	abstract double getArea();
 
+	/**
+	 * @return the cornerCnt
+	 */
+	public int getCornerCnt() {
+		return cornerCnt;
+	}
+
+	/**
+	 * @param cornerCnt the cornerCnt to set
+	 */
+	public void setCornerCnt(int cornerCnt) {
+		this.cornerCnt = cornerCnt;
+	}
 }
