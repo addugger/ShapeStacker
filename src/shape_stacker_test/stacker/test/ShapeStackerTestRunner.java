be54c557add2350a743ca538e5e3249b9f359bf3
Aaron Dugger <addugger@gmail.com>
2016-08-28 17:46:02 -0400

    Clean up indents in ShapeStackerTestRunner.java

--- src/shape_stacker_test/stacker/test/ShapeStackerTestRunner.java
+++ src/shape_stacker_test/stacker/test/ShapeStackerTestRunner.java
@@ -1,3 +1,7 @@
+/**
+ * Just a simple class to run the ShapeStackerTest tests. This class
+ * contians the only main method in the entire ShapeStacker test project.
+ */
 package shape_stacker_test.stacker.test;
 
 import org.junit.runner.JUnitCore;
@@ -5,13 +9,14 @@ import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 public class ShapeStackerTestRunner {
-   public static void main(String[] args) {
-      Result result = JUnitCore.runClasses(ShapeStackerTest.class);
-		
-      for (Failure failure : result.getFailures()) {
-         System.out.println("Failure: " + failure.toString());
-      }
-		
-      System.out.println("All tests succeeded: " + result.wasSuccessful());
-   }
+	
+	public static void main(String[] args) {
+		Result result = JUnitCore.runClasses(ShapeStackerTest.class);
+		// Print out failures
+		for (Failure failure : result.getFailures()) {
+			System.out.println("Failure: " + failure.toString());
+		}
+		// Print out whether the tests all succeeded
+		System.out.println("All tests succeeded: " + result.wasSuccessful());
+	}
 } 
